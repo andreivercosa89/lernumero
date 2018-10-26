84b87402e2e808767c23d0910863dbca98df8e2c
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-10-26 19:07:34 -0400

    Ant e suc em objeto

---
+++ lernumero/Calcular.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace lernumero
+{
+    public class Calcular
+    {
+
+        private int numero;
+
+        public void definiNumero(int valor)
+        {
+            numero = valor;
+        }
+        public int getValor()
+        {
+            return numero;
+        }
+
+        public int antecessor()
+        {
+            return (numero-1);
+        }
+
+        public int sucessor()
+        {
+            return (numero+1);
+        }
+    }
+}
+
---
+++ lernumero/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lernumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorDigitado;
+            Console.WriteLine("Informe um numero: ");
+            Calcular calcular = new Calcular();
+
+            calcular.definiNumero(Int32.Parse(Console.ReadLine()));
+
+
+            Console.WriteLine("Antecessor: " + calcular.antecessor() + "\nSucessor: " + calcular.sucessor());
+        }
+    }
+}
